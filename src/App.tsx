abd0ca121437762bc3afd223e1f1971ef0d66854
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-01-08 10:33:55 +0700

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import './App.css';
 const App: FC = () => {
   return (
     <div className='App'>
+      <div className='header'>
+        <div className='inputContainer'>
+          <input type="text" placeholder='Task...' />
+          <input type="number" placeholder='Deadline (in Days)...' />
+        </div>
+        <button className='btnAdd'>Add Task</button>
+      </div>
+      <div className='todoList'></div>
     </div>
   );
 }
